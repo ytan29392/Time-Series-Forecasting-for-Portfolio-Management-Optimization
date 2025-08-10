7f2290cbf4b8a60691197998c9c6e5bca6951e3f
ytan29392 <ytan29392@gmail.com>
2025-08-10 23:32:43 +0300

    Update on dowload dataset

--- scripts/download_data.py
+++ scripts/download_data.py
@@ -1,6 +1,6 @@
 import yfinance as yf
 import pandas as pd
-tickers = ["SPY", "AAPL", "BTC-USD"]  # Change to your asset (AAPL, BTC-USD, etc.)
+tickers = ["SPY", "TSLA", "BND"]
 
 for ticker in tickers:
   data = yf.download(ticker, start="2015-01-01", end="2024-12-31")
